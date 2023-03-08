5b2f3d96e662fcc98ceeb8dc733105be6b7e0d08
Tuna Öztürk <business@tunaozturk.com>
2023-03-08 23:47:22 +0300

    Test Commit 2

--- Neon/Package.swift
+++ Neon/Package.swift
@@ -19,7 +19,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Neon"/*,
+            name: "Neon",
+            path: "Sources"/*,
             dependencies: []*/),
         .testTarget(
             name: "NeonTests",
