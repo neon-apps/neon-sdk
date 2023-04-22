997012c077b443c6cb9eb0799f9f71227c27b7a5
Tunahan Öztürk <business@tunaozturk.com>
2023-04-22 19:20:57 +0300

    NeonViewController added

---
+++ Neon/Core/Custom Classes/UI Elements/NeonViewController.swift
@@ -0,0 +1,48 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tuna Öztürk on 22.04.2023.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+class NeonViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    fileprivate let scrollView = UIScrollView()
+    public let mainStack = UIStackView()
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addMainStack()
+    }
+    
+    // MARK: - UI Creation
+    
+    private func addMainStack() {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        scrollView.addSubview(mainStack)
+        scrollView.contentInsetAdjustmentBehavior = .never
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        mainStack.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalTo(view.snp.width)
+        }
+        mainStack.axis = .vertical
+        mainStack.spacing = 20
+    }
+    
+    // MARK: - Spacing Functions
+    
+}
---
+++ Neon/Core/Extensions/UIStackView+Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tuna Öztürk on 22.04.2023.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+extension UIStackView{
+    
+    func addSpacer(_ size: CGFloat = 20) {
+        let spacingView = UIView()
+        spacingView.layer.borderColor = UIColor.green.cgColor
+        spacingView.layer.borderWidth = 3
+        spacingView.snp.makeConstraints { make in
+            make.height.width.equalTo(size - self.spacing)
+        }
+        self.addArrangedSubview(spacingView)
+    }
+}
