829a731546106dd8d4f2cb8540227dde01e53f11
Tunahan Öztürk <business@tunaozturk.com>
2023-07-01 13:24:55 +0300

    Bug Fixes VisionOS

--- Package.swift
+++ Package.swift
@@ -3,10 +3,8 @@
 
 import PackageDescription
 
-#if !os(xrOS)
 let package = Package(
-    name: "Neon",
-    products: [
+    name: "Neon",    products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "NeonSDK",
@@ -14,6 +12,7 @@ let package = Package(
     ],
     
     dependencies: [
+        
         .package(name: "SDWebImage", url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.15.5"),
         .package(name: "RevenueCat", url: "https://github.com/RevenueCat/purchases-ios.git", from: "4.17.7"),
         .package(name: "Lottie", url: "https://github.com/airbnb/lottie-spm.git", from: "4.1.3"),
@@ -31,22 +30,20 @@ let package = Package(
        
         .target(
             name: "NeonSDK",  dependencies: [
-             
                 "Lottie",
-           
                 "RevenueCat",
                 "SDWebImage",
                 "Hero",
                 "SnapKit",
                 "Localize_Swift",
                 "Adapty",
-                .product(name: "FirebaseAnalytics", package: "Firebase"),
-                .product(name: "FirebaseRemoteConfig", package: "Firebase"),
-                .product(name: "FirebaseStorage", package: "Firebase"),
-                .product(name: "FirebaseFirestore", package: "Firebase"),
-                .product(name: "FirebaseFirestoreSwift", package: "Firebase"),
-                .product(name: "FirebaseAuth", package: "Firebase"),
-                .product(name: "FirebaseCrashlytics", package: "Firebase")
+                .product(name: "FirebaseAnalytics", package: "Firebase", condition: .when(platforms: [.iOS])),
+                .product(name: "FirebaseRemoteConfig", package: "Firebase", condition: .when(platforms: [.iOS])),
+                .product(name: "FirebaseStorage", package: "Firebase", condition: .when(platforms: [.iOS])),
+                .product(name: "FirebaseFirestore", package: "Firebase", condition: .when(platforms: [.iOS])),
+                .product(name: "FirebaseFirestoreSwift", package: "Firebase", condition: .when(platforms: [.iOS])),
+                .product(name: "FirebaseAuth", package: "Firebase", condition: .when(platforms: [.iOS])),
+                .product(name: "FirebaseCrashlytics", package: "Firebase", condition: .when(platforms: [.iOS]))
                 /* /x"
                 .product(name: "FirebaseDynamicLinks", package: "Firebase"),
                 
@@ -55,47 +52,3 @@ let package = Package(
         
     ]
 )
-
-#else
-let package = Package(
-    name: "Neon",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "NeonSDK",
-            targets: ["NeonSDK"])
-    ],
-    
-    dependencies: [
-        .package(name: "SDWebImage", url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.15.5"),
-        .package(name: "RevenueCat", url: "https://github.com/RevenueCat/purchases-ios.git", from: "4.17.7"),
-        .package(name: "Hero", url: "https://github.com/HeroTransitions/Hero.git", .upToNextMajor(from: "1.4.0")),
-        .package(name: "SnapKit", url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
-        .package(name: "Localize_Swift", url: "https://github.com/marmelroy/Localize-Swift.git", .upToNextMajor(from: "3.2.0")),
-        .package(name: "Adapty", url: "https://github.com/adaptyteam/AdaptySDK-iOS.git", .upToNextMajor(from: "2.6.0"))
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-       
-        .target(
-            name: "NeonSDK",  dependencies: [
-                "RevenueCat",
-                "SDWebImage",
-                "Hero",
-                "SnapKit",
-                "Localize_Swift",
-                "Adapty",
-                /* /x"
-                .product(name: "FirebaseDynamicLinks", package: "Firebase"),
-                
-               */
-            ], path: "Neon", resources: [.process("Core/Resources/Animations"), .process("Core/Resources/Fonts"), .process("Core/Resources/Assets")])
-        
-    ]
-)
-
-#endif
-
