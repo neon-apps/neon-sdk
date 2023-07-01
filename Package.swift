ffb27a9be1b1c694a943a0f45ab1701f100ced24
Tunahan Öztürk <business@tunaozturk.com>
2023-07-01 13:38:15 +0300

    Bug Fixes

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
                 "Hero",
                 "SnapKit",
                 "Localize_Swift",
-                "Adapty",
+                .product(name: "Adapty", package: "Adapty", condition: .when(platforms: [.iOS])),
                 .product(name: "SDWebImage", package: "SDWebImage", condition: .when(platforms: [.iOS])),
                 .product(name: "Lottie", package: "Lottie", condition: .when(platforms: [.iOS])),
                 .product(name: "FirebaseAnalytics", package: "Firebase", condition: .when(platforms: [.iOS])),
