63d1205aa4b657d0fd90a38d150f3506460a865e
Tunahan Öztürk <business@tunaozturk.com>
2023-09-22 16:39:10 +0300

    Bug Fixes on NeonViewController

--- Neon/Core/Custom Classes/UI Elements/NeonViewController.swift
+++ Neon/Core/Custom Classes/UI Elements/NeonViewController.swift
@@ -10,62 +10,49 @@ import UIKit
 import SnapKit
 
 open class NeonViewController: UIViewController {
-    
     // MARK: - Properties
-    
-    fileprivate let scrollView = UIScrollView()
+    public var scrollView = UIScrollView()
     public let mainStack = UIStackView()
     public var padding = UIEdgeInsets.zero{
         didSet{
             applyPadding()
         }
     }
-    
-    
     // MARK: - View Lifecycle
-    
-    
     open override func viewDidLoad() {
         super.viewDidLoad()
         addMainStack()
     }
-    
     // MARK: - UI Creation
-    
     private func addMainStack() {
         view.addSubview(scrollView)
         scrollView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
             make.edges.equalToSuperview()
         }
-        
         scrollView.addSubview(mainStack)
         scrollView.contentInsetAdjustmentBehavior = .never
         scrollView.showsVerticalScrollIndicator = false
         scrollView.showsHorizontalScrollIndicator = false
         mainStack.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-            make.width.equalTo(view.snp.width)
+            make.centerX.equalToSuperview()
+            make.top.right.bottom.left.equalToSuperview()
         }
         mainStack.axis = .vertical
         mainStack.spacing = 20
-        
     }
-    
     func applyPadding(){
-        mainStack.layoutMargins = UIEdgeInsets(top: padding.top, left: 0, bottom: padding.bottom, right: 0)
+        mainStack.layoutMargins = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         mainStack.isLayoutMarginsRelativeArrangement = true
         mainStack.snp.remakeConstraints { make in
-            make.left.equalToSuperview().offset(padding.left)
-            make.right.equalToSuperview().inset(padding.right)
-            make.top.equalToSuperview()
-            make.bottom.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.leading.trailing.top.bottom.equalToSuperview()
             
         }
     }
-    
-    }
+}
+// MARK: - Spacing Functions
 
+// MARK: - Spacing Functions
 
-    // MARK: - Spacing Functions
-    
 
