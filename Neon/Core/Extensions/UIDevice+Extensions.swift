a25907f34a06369f63bea50367d9fe918fa76292
Tunahan Öztürk <business@tunaozturk.com>
2023-10-22 17:18:33 +0300

    Add DeviceModel enum

--- Neon/Core/Extensions/UIDevice+Extensions.swift
+++ Neon/Core/Extensions/UIDevice+Extensions.swift
@@ -117,3 +117,192 @@ public extension UIDevice {
     }()
     
 }
+
+
+public enum DeviceModel: String {
+    // iPhone
+    case iPhone4
+    case iPhone4s
+    case iPhone5
+    case iPhone5c
+    case iPhone5s
+    case iPhone6
+    case iPhone6Plus
+    case iPhone6s
+    case iPhone6sPlus
+    case iPhoneSE
+    case iPhoneSE2
+    case iPhoneSE3
+    case iPhone7
+    case iPhone7Plus
+    case iPhone8
+    case iPhone8Plus
+    case iPhoneX
+    case iPhoneXS
+    case iPhoneXSMax
+    case iPhoneXR
+    case iPhone11
+    case iPhone11Pro
+    case iPhone11ProMax
+    case iPhone12Mini
+    case iPhone12
+    case iPhone12Pro
+    case iPhone12ProMax
+    case iPhone13Mini
+    case iPhone13
+    case iPhone13Pro
+    case iPhone13ProMax
+    case iPhone14
+    case iPhone14Plus
+    case iPhone14Pro
+    case iPhone14ProMax
+    case iPhone15
+    case iPhone15Plus
+    case iPhone15Pro
+    case iPhone15ProMax
+    
+    // iPod Touch
+    case iPodTouch5
+    case iPodTouch6
+    case iPodTouch7
+    
+    // iPad
+    case iPad2
+    case iPad3
+    case iPad4
+    case iPad5
+    case iPad6
+    case iPad7
+    case iPad8
+    case iPad9
+    case iPad10
+    case iPadAir
+    case iPadAir2
+    case iPadAir3
+    case iPadAir4
+    case iPadAir5
+    case iPadMini
+    case iPadMini2
+    case iPadMini3
+    case iPadMini4
+    case iPadMini5
+    case iPadMini6
+    case iPadPro9_7
+    case iPadPro10_5
+    case iPadPro11_1stGen
+    case iPadPro11_2ndGen
+    case iPadPro11_3rdGen
+    case iPadPro11_4thGen
+    case iPadPro11_5thGen
+    case iPadPro12_9_1stGen
+    case iPadPro12_9_2ndGen
+    case iPadPro12_9_3rdGen
+    case iPadPro12_9_4thGen
+    case iPadPro12_9_5thGen
+    case iPadPro12_9_6thGen
+    
+    // Apple TV
+    case appleTV
+    case appleTV4K
+    
+    // HomePod
+    case homePod
+    case homePodMini
+    
+    // Simulator
+    case simulator
+    
+    // Unknown
+    case unknown // Default case
+}
+
+public extension UIDevice {
+    
+    // Get the model name of the current device
+    static var currentDeviceModel: DeviceModel {
+        let modelName = UIDevice.modelName
+        
+        switch modelName {
+        case "iPod5,1": return .iPodTouch5
+        case "iPod7,1": return .iPodTouch6
+        case "iPod9,1": return .iPodTouch7
+        case "iPhone3,1", "iPhone3,2", "iPhone3,3": return .iPhone4
+        case "iPhone4,1": return .iPhone4s
+        case "iPhone5,1", "iPhone5,2": return .iPhone5
+        case "iPhone5,3", "iPhone5,4": return .iPhone5c
+        case "iPhone6,1", "iPhone6,2": return .iPhone5s
+        case "iPhone7,2": return .iPhone6
+        case "iPhone7,1": return .iPhone6Plus
+        case "iPhone8,1": return .iPhone6s
+        case "iPhone8,2": return .iPhone6sPlus
+        case "iPhone9,1", "iPhone9,3": return .iPhone7
+        case "iPhone9,2", "iPhone9,4": return .iPhone7Plus
+        case "iPhone10,1", "iPhone10,4": return .iPhone8
+        case "iPhone10,2", "iPhone10,5": return .iPhone8Plus
+        case "iPhone10,3", "iPhone10,6": return .iPhoneX
+        case "iPhone11,2": return .iPhoneXS
+        case "iPhone11,4", "iPhone11,6": return .iPhoneXSMax
+        case "iPhone11,8": return .iPhoneXR
+        case "iPhone12,1": return .iPhone11
+        case "iPhone12,3": return .iPhone11Pro
+        case "iPhone12,5": return .iPhone11ProMax
+        case "iPhone13,1": return .iPhone12Mini
+        case "iPhone13,2": return .iPhone12
+        case "iPhone13,3": return .iPhone12Pro
+        case "iPhone13,4": return .iPhone12ProMax
+        case "iPhone14,4": return .iPhone13Mini
+        case "iPhone14,5": return .iPhone13
+        case "iPhone14,2": return .iPhone13Pro
+        case "iPhone14,3": return .iPhone13ProMax
+        case "iPhone14,7": return .iPhone14
+        case "iPhone14,8": return .iPhone14Plus
+        case "iPhone15,2": return .iPhone14Pro
+        case "iPhone15,3": return .iPhone14ProMax
+        case "iPhone15,4": return .iPhone15
+        case "iPhone15,5": return .iPhone15Plus
+        case "iPhone16,1": return .iPhone15Pro
+        case "iPhone16,2": return .iPhone15ProMax
+        case "iPhone8,4": return .iPhoneSE
+        case "iPhone12,8": return .iPhoneSE2
+        case "iPhone14,6": return .iPhoneSE3
+        case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4": return .iPad2
+        case "iPad3,1", "iPad3,2", "iPad3,3": return .iPad3
+        case "iPad3,4", "iPad3,5", "iPad3,6": return .iPad4
+        case "iPad6,11", "iPad6,12": return .iPad5
+        case "iPad7,5", "iPad7,6": return .iPad6
+        case "iPad7,11", "iPad7,12": return .iPad7
+        case "iPad11,6", "iPad11,7": return .iPad8
+        case "iPad12,1", "iPad12,2": return .iPad9
+        case "iPad13,18", "iPad13,19": return .iPad10
+        case "iPad4,1", "iPad4,2", "iPad4,3": return .iPadAir
+        case "iPad5,3", "iPad5,4": return .iPadAir2
+        case "iPad11,3", "iPad11,4": return .iPadAir3
+        case "iPad13,1", "iPad13,2": return .iPadAir4
+        case "iPad13,16", "iPad13,17": return .iPadAir5
+        case "iPad2,5", "iPad2,6", "iPad2,7": return .iPadMini
+        case "iPad4,4", "iPad4,5", "iPad4,6": return .iPadMini2
+        case "iPad4,7", "iPad4,8", "iPad4,9": return .iPadMini3
+        case "iPad5,1", "iPad5,2": return .iPadMini4
+        case "iPad11,1", "iPad11,2": return .iPadMini5
+        case "iPad14,1", "iPad14,2": return .iPadMini6
+        case "iPad6,3", "iPad6,4": return .iPadPro9_7
+        case "iPad7,3", "iPad7,4": return .iPadPro10_5
+        case "iPad8,1", "iPad8,2", "iPad8,3", "iPad8,4": return .iPadPro11_1stGen
+        case "iPad8,9", "iPad8,10": return .iPadPro11_2ndGen
+        case "iPad13,4", "iPad13,5", "iPad13,6", "iPad13,7": return .iPadPro11_3rdGen
+        case "iPad14,3", "iPad14,4": return .iPadPro11_4thGen
+        case "iPad6,7", "iPad6,8": return .iPadPro12_9_1stGen
+        case "iPad7,1", "iPad7,2": return .iPadPro12_9_2ndGen
+        case "iPad8,5", "iPad8,6", "iPad8,7", "iPad8,8": return .iPadPro12_9_3rdGen
+        case "iPad8,11", "iPad8,12": return .iPadPro12_9_4thGen
+        case "iPad13,8", "iPad13,9", "iPad13,10", "iPad13,11": return .iPadPro12_9_5thGen
+        case "iPad14,5", "iPad14,6": return .iPadPro12_9_6thGen
+        case "AppleTV5,3": return .appleTV
+        case "AppleTV6,2": return .appleTV4K
+        case "AudioAccessory1,1": return .homePod
+        case "AudioAccessory5,1": return .homePodMini
+        case "i386", "x86_64", "arm64": return .simulator
+        default: return .unknown // Return unknown for unrecognized models
+        }
+    }
+}
