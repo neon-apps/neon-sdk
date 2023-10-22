c3419d5beecfce7084743ca435a1474c1a372d91
Tunahan Öztürk <business@tunaozturk.com>
2023-10-22 12:10:11 +0300

    Bug fixes

--- Neon/Core/Helpers/NeonAlertManager.swift
+++ Neon/Core/Helpers/NeonAlertManager.swift
@@ -10,10 +10,10 @@ import Foundation
 @available(iOS 13.0, *)
 public class NeonAlertManager {
     // Static property to access DefaultAlertManager
-    public static let `default` = DefaultAlertManager()
+    public static let `default` = DefaultAlertManager.self
 
     // Static property to access CustomAlertManager
-    public static let custom = CustomAlertManager()
+    public static let custom = CustomAlertManager.self
 
     // Private initializer to prevent external instantiation
     private init() { }
