59bc5c09f1d47669196e6e7092d19da31f92283a
Tunahan Öztürk <business@tunaozturk.com>
2023-12-22 12:36:08 +0300

    NeonNotificationCenter remove observer function added

--- Neon/Core/Extensions/NotificationCenter+Extensions.swift
+++ Neon/Core/Extensions/NotificationCenter+Extensions.swift
@@ -9,8 +9,8 @@ import Foundation
 
 extension NotificationCenter {
 
-    public class func observe(name: NSNotification.Name, handler: @escaping (Notification) -> Void) {
-        self.default.addObserver(forName: name, object: nil, queue: .main, using: handler)
+    public class func observe(name: NSNotification.Name, handler: @escaping (Notification) -> Void) -> NSObject{
+        return self.default.addObserver(forName: name, object: nil, queue: .main, using: handler) as! NSObject
     }
     
     
--- Neon/Core/Helpers/NeonNotificationCenter.swift
+++ Neon/Core/Helpers/NeonNotificationCenter.swift
@@ -10,6 +10,7 @@ import Foundation
 
 public class NeonNotificationCenter{
     
+    private static var activeObservers = [(NSObject, String)]()
     public static func post(id : String, object : Any? = nil){
         
         if let object{
@@ -21,18 +22,26 @@ public class NeonNotificationCenter{
     }
     
     public static func observe(id : String, completion: @escaping (_ object : Any?) -> ()){
-        NotificationCenter.observe(name: Notification.Name(rawValue: id)) { notification in
+        var notificationObject = NotificationCenter.observe(name: Notification.Name(rawValue: id)) { notification in
             if let userInfo = notification.userInfo as? [String : Any], let object = userInfo["neon-object"]{
                     completion(object)
             }else{
                 completion(nil)
             }
         }
+        activeObservers.append((notificationObject,id))
     }
     
     public static func observe(id : String, completion: @escaping () -> ()){
-        NotificationCenter.observe(name: Notification.Name(rawValue: id)) { notification in
+        var notificationObject =  NotificationCenter.observe(name: Notification.Name(rawValue: id)) { notification in
             completion()
+        }
+        activeObservers.append((notificationObject,id))
+    }
+    
+    public static func removeAllObservers(){
+        for observer in activeObservers {
+            NotificationCenter.removeObserver(observer.0, forKeyPath: observer.1)
         }
     }
     
