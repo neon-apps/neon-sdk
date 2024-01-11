7ee7b1be87a55a14938b2b92c8f0834eb233a443
Tunahan Öztürk <business@tunaozturk.com>
2024-01-11 19:54:54 +0300

    Create UIColor+Extensions.swift

---
+++ Neon/Core/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,42 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tuna Öztürk on 11.01.2024.
+//
+
+import Foundation
+import UIKit
+extension UIColor {
+    static func fromHex(_ hex: String) -> UIColor? {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgb & 0x0000FF) / 255.0
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+}
+
+extension UIColor {
+    func toHex() -> String {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+
+        self.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+
+        let redInt = Int(red * 255.0)
+        let greenInt = Int(green * 255.0)
+        let blueInt = Int(blue * 255.0)
+
+        return String(format: "#%02X%02X%02X", redInt, greenInt, blueInt)
+    }
+}
