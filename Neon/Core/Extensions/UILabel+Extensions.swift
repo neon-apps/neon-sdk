93c9d31e915e316e73f628ac1133f6194c0bfe96
Tunahan Öztürk <business@tunaozturk.com>
2024-01-20 14:53:35 +0300

    Update UILabel+Extensions.swift

--- Neon/Core/Extensions/UILabel+Extensions.swift
+++ Neon/Core/Extensions/UILabel+Extensions.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Tuna Öztürk on 11.03.2023.
 //
@@ -18,7 +18,7 @@ extension UILabel {
                                       range: NSRange(location: 0, length: attributedString.length))
         self.attributedText = attributedString
     }
-
+    
     public func setTextWithLeftImage(image: UIImage, text: String) {
         let attachment = NSTextAttachment()
         attachment.image = image
@@ -40,6 +40,7 @@ public enum AttributeType {
     case kerning(CGFloat)
     case lineSpacing(CGFloat)
     case shadow(NSShadow)
+    case action(() -> Void)
 }
 
 extension UILabel {
@@ -72,9 +73,67 @@ extension UILabel {
                 mutableAttributedText.addAttribute(.paragraphStyle, value: paragraphStyle, range: range)
             case .shadow(let shadow):
                 mutableAttributedText.addAttribute(.shadow, value: shadow, range: range)
+            case .action(let action):
+                let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+                self.isUserInteractionEnabled = true
+                self.addGestureRecognizer(tapGestureRecognizer)
+                self.actionForRange[range] = action
             }
             
             self.attributedText = mutableAttributedText
         }
     }
+    
+    private var actionForRange: [NSRange: () -> Void] {
+        get {
+            if let dictionary = objc_getAssociatedObject(self, &AssociatedKeys.actionForRange) as? [NSRange: () -> Void] {
+                return dictionary
+            }
+            return [:]
+        }
+        set {
+            objc_setAssociatedObject(self, &AssociatedKeys.actionForRange, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
+    
+    
+    @objc private func handleTap(_ gestureRecognizer: UITapGestureRecognizer) {
+        let tapLocation = gestureRecognizer.location(in: self)
+        if let (word, range) = wordAtLocation(tapLocation) {
+            if let action = actionForRange[range] {
+                action()
+            }
+        }
+    }
+    
+    private func wordAtLocation(_ location: CGPoint) -> (String, NSRange)? {
+        let layoutManager = NSLayoutManager()
+        let textContainer = NSTextContainer(size: CGSize.zero)
+        let textStorage = NSTextStorage(attributedString: self.attributedText ?? NSAttributedString())
+        
+        layoutManager.addTextContainer(textContainer)
+        textStorage.addLayoutManager(layoutManager)
+        
+        textContainer.lineFragmentPadding = 0
+        textContainer.lineBreakMode = self.lineBreakMode
+        textContainer.maximumNumberOfLines = self.numberOfLines
+        textContainer.size = self.bounds.size
+        
+        let characterIndex = layoutManager.characterIndex(for: location, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+        
+        for (range, _) in actionForRange {
+            if NSLocationInRange(characterIndex, range) {
+                let word = (self.text as NSString?)?.substring(with: range)
+                return (word ?? "", range)
+            }
+        }
+        
+        return nil
+    }
+    
+    private struct AssociatedKeys {
+        static var actionForRange = "actionForRange"
+    }
 }
+
