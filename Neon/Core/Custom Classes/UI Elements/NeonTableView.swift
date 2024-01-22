275ed8762e5ca2e7d35b6f1519c6567f114ee3e9
Tunahan Öztürk <business@tunaozturk.com>
2024-01-22 21:25:46 +0300

    Update NeonTableView.swift

--- Neon/Core/Custom Classes/UI Elements/NeonTableView.swift
+++ Neon/Core/Custom Classes/UI Elements/NeonTableView.swift
@@ -159,6 +159,9 @@ open class NeonTableView<T, Cell: NeonTableViewCell<T>>: UITableView, UITableVie
         cell.setTemplateWithSubviews(isShimmerActive, animate: true, viewBackgroundColor: ShimmerManager.shimmerBackgroundColor)
     }
     
+    open func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        return proposedDestinationIndexPath
+    }
    
 }
 
