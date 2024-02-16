9ccc986fee060e6c6eeedbedd0b3c1db0c01bee1
Tunahan Öztürk <business@tunaozturk.com>
2024-02-16 10:54:20 +0300

    Update StorageManager.swift

--- Neon/Firebase Manager/Storage Manager/StorageManager.swift
+++ Neon/Firebase Manager/Storage Manager/StorageManager.swift
@@ -36,5 +36,33 @@ public class StorageManager{
         
     }
     
+    public static func uploadImage(image: UIImage, compressionRatio: CGFloat, storageRef: String? = nil, completion: @escaping (_ downloadURL: String?) -> ()) {
+        
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        // Compress image using JPEG compression with the provided quality parameter
+        guard let data = image.jpegData(compressionQuality: compressionRatio) else {
+            completion(nil) // Handle error or invalid compression quality
+            return
+        }
+        let imageID = UUID().uuidString
+        let imageRef = storageReference.child("\(storageRef ?? imageID).jpg")
+        
+        let newMetadata = StorageMetadata()
+        newMetadata.contentType = "image/jpeg"
+        
+        _ = imageRef.putData(data, metadata: newMetadata) { metadata, error in
+            imageRef.downloadURL { url, error in
+                guard let downloadURL = url else {
+                    // Uh-oh, an error occurred!
+                    completion(nil)
+                    return
+                }
+                completion(downloadURL.absoluteString)
+            }
+        }
+    }
+
+    
 }
 #endif
