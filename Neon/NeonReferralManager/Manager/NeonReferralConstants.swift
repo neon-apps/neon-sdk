b9254c5d8da610994a90b97c78bed5273b7531a3
Tunahan Öztürk <business@tunaozturk.com>
2024-08-10 13:25:26 +0300

    NeonReferralManager Added

---
+++ Neon/NeonReferralManager/Manager/NeonReferralConstants.swift
@@ -0,0 +1,34 @@
+//
+//  NeonReferralConstants.swift
+//  PromotionCode
+//
+//  Created by cihangirincaz on 6.08.2024.
+//
+
+import Foundation
+import UIKit
+
+class NeonReferralConstants{
+    static let shared = NeonReferralConstants()
+    //MARK: remaining credit
+    static var remainingCredit = 0
+    //MARK: temp array
+    var AllUser: [String] = []
+    var newAllUsers: [String] = []
+    //MARK: configure
+    static var amount = Int()
+    static var lottieName =  String()
+    static var freeText = String()
+    static var thirdLabelText = String()
+    static var descriptionLabelText = String()
+    static var remainingBackgroundColor = UIColor()
+    static var mainTextColor = UIColor()
+    static var mainColor = UIColor()
+    static var descriptionColor = UIColor()
+    static var gradientTopColor = UIColor()
+    static var backgroundColor = UIColor()
+    static var textFieldbackgroundcolor = UIColor()
+    static var textFieldBordorColor = UIColor()
+    static var buttonTextColor = UIColor()
+    static var sheetBackgroundColor = UIColor()
+}
---
+++ Neon/NeonReferralManager/Screens/CodeViewVC/Controller/CodeViewVC.swift
@@ -0,0 +1,302 @@
+//
+//  CodeViewVC.swift
+//  PromotionCode
+//
+//  Created by cihangirincaz on 30.07.2024.
+//
+
+import UIKit
+import NeonSDK
+import FirebaseFirestore
+
+class CodeViewVC: UIViewController {
+    //MARK: Properties
+    var newUserID = String()
+    let remainingCredit = PaddedLabel()
+    private var notificationView = UIView()
+    let copyButton = UIButton()
+    //MARK: Lifecycle
+    init(){
+           super.init(nibName: nil, bundle: nil)
+    }
+    required init?(coder aDecoder: NSCoder) {
+           fatalError("init(coder:) has not been implemented")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupUI()
+        self.gradientConfigure()
+        self.setupNotificationView()
+        NeonReferralDatabase.shared.fetchUserCredit() { credit, error in
+               if let error = error {
+                   print("Error fetching document: \(error)")
+               } else if let credit = credit {
+                   print("User credit : \(credit)")
+                   NeonReferralConstants.remainingCredit = credit
+                   self.fetchCredit()
+               } else {
+                   print("Document does not exist or `Credit` field is missing.")
+               }
+           }
+    }
+    //MARK: Helpers
+    func setupUI(){
+        view.backgroundColor = NeonReferralConstants.backgroundColor
+        let backButton = UIButton()
+        backButton.setImage(.back , for: .normal)
+        backButton.addTarget(self, action: #selector(backButtonClicked), for: .touchUpInside)
+        view.addSubview(backButton)
+        backButton.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(18)
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.height.width.equalTo(40)
+        }
+        remainingCredit.text = "Remaining Invite Credits: 0 "
+        remainingCredit.font = Font.custom(size: 14, fontWeight: .Regular)
+        remainingCredit.layer.masksToBounds = true
+        remainingCredit.layer.cornerRadius = 8
+        remainingCredit.backgroundColor = NeonReferralConstants.remainingBackgroundColor
+        remainingCredit.textAlignment = .center
+        remainingCredit.textColor = NeonReferralConstants.mainTextColor
+        remainingCredit.textInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        view.addSubview(remainingCredit)
+        remainingCredit.snp.makeConstraints { make in
+            make.centerY.equalTo(backButton.snp.centerY)
+            make.right.equalToSuperview().inset(18)
+            make.height.equalTo(34)
+            make.width.equalTo(206)
+        }
+        let showLottie = LottieManager.createLottie(animation: .custom(name: NeonReferralConstants.lottieName))
+        view.addSubview(showLottie)
+        showLottie.snp.makeConstraints { make in
+            make.top.equalTo(remainingCredit.snp.bottom).offset(view.frame.height*0.043)
+            make.centerX.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.12)
+        }
+        let inviteFriendsLabel = UILabel()
+        inviteFriendsLabel.text = "Invite Friends"
+        inviteFriendsLabel.textColor = NeonReferralConstants.mainTextColor
+        inviteFriendsLabel.backgroundColor = .clear
+        inviteFriendsLabel.font = Font.custom(size: 32, fontWeight: .Medium)
+        view.addSubview(inviteFriendsLabel)
+        inviteFriendsLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(showLottie.snp.bottom).offset(view.frame.height*0.034)
+            make.height.equalTo(47)
+        }
+        let freeAny = UILabel()
+        freeAny.text = NeonReferralConstants.freeText
+        freeAny.textColor = NeonReferralConstants.mainColor
+        freeAny.backgroundColor = .clear
+        freeAny.font = Font.custom(size: 40, fontWeight: .Medium)
+        view.addSubview(freeAny)
+        freeAny.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(inviteFriendsLabel.snp.bottom).offset(0)
+            make.height.equalTo(47)
+        }
+        let firstLabel = UILabel()
+        firstLabel.text = "1. Share your code with your friends"
+        firstLabel.font = Font.custom(size: 16, fontWeight: .Regular)
+        firstLabel.textColor = NeonReferralConstants.mainTextColor
+        view.addSubview(firstLabel)
+        firstLabel.snp.makeConstraints { make in
+            make.top.equalTo(freeAny.snp.bottom).offset(view.frame.height*0.034)
+            make.left.equalToSuperview().inset(35)
+            make.height.equalTo(24)
+        }
+        let firstImageView = UIImageView(image: .selfie)
+        firstImageView.contentMode = .scaleAspectFit
+        firstImageView.tintColor = .white
+        view.addSubview(firstImageView)
+        firstImageView.snp.makeConstraints { make in
+            make.left.equalTo(firstLabel.snp.right)
+            make.centerY.equalTo(firstLabel.snp.centerY)
+            make.height.equalTo(20)
+        }
+        let secondLabel = UILabel()
+        secondLabel.text = "2. They enter your code in Settings"
+        secondLabel.font = Font.custom(size: 16, fontWeight: .Regular)
+        secondLabel.textColor = NeonReferralConstants.mainTextColor
+        view.addSubview(secondLabel)
+        secondLabel.snp.makeConstraints { make in
+            make.top.equalTo(firstLabel.snp.bottom).offset(view.frame.height*0.019)
+            make.left.equalToSuperview().inset(35)
+            make.height.equalTo(24)
+        }
+        let secondImageView = UIImageView(image: .phone)
+        secondImageView.contentMode = .scaleAspectFit
+        secondImageView.tintColor = .white
+        view.addSubview(secondImageView)
+        secondImageView.snp.makeConstraints { make in
+            make.left.equalTo(secondLabel.snp.right).offset(4)
+            make.centerY.equalTo(secondLabel.snp.centerY)
+            make.height.equalTo(20)
+        }
+        let thirdLabel = UILabel()
+        thirdLabel.text = NeonReferralConstants.thirdLabelText
+        thirdLabel.font = Font.custom(size: 16, fontWeight: .Regular)
+        thirdLabel.textColor = NeonReferralConstants.mainTextColor
+        view.addSubview(thirdLabel)
+        thirdLabel.snp.makeConstraints { make in
+            make.top.equalTo(secondLabel.snp.bottom).offset(view.frame.height*0.019)
+            make.left.equalToSuperview().inset(35)
+            make.height.equalTo(24)
+        }
+        let thirdImageView = UIImageView(image: .group)
+        thirdImageView.contentMode = .scaleAspectFit
+        thirdImageView.tintColor = .white
+        view.addSubview(thirdImageView)
+        thirdImageView.snp.makeConstraints { make in
+            make.left.equalTo(thirdLabel.snp.right).offset(4)
+            make.centerY.equalTo(thirdLabel.snp.centerY)
+            make.height.equalTo(20)
+        }
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = NeonReferralConstants.descriptionLabelText
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.font = Font.custom(size: 14, fontWeight: .Regular)
+        descriptionLabel.textAlignment = .center
+        descriptionLabel.textColor = NeonReferralConstants.descriptionColor
+        view.addSubview(descriptionLabel)
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(thirdLabel.snp.bottom).offset(view.frame.height*0.042)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(38)
+            make.width.equalToSuperview().multipliedBy(0.9)
+        }
+        let userCodeLabel = UILabel()
+        let ReferanceCodeLabel = PaddedLabel()
+        let inviteButton = UIButton()
+        inviteButton.backgroundColor = NeonReferralConstants.mainColor
+        inviteButton.setTitle("Invite", for: .normal)
+        inviteButton.setTitleColor(NeonReferralConstants.buttonTextColor, for: .normal)
+        inviteButton.layer.masksToBounds = true
+        inviteButton.layer.cornerRadius = 16
+        inviteButton.addTarget(self, action: #selector(inviteButtonClicked), for: .touchUpInside)
+        view.addSubview(inviteButton)
+        inviteButton.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(view.frame.height*0.07)
+            make.width.equalToSuperview().multipliedBy(0.9)
+        }
+        guard let userID = AuthManager.currentUserID else { return }
+        newUserID = String(userID.prefix(6))
+        ReferanceCodeLabel.text = newUserID
+        ReferanceCodeLabel.textColor = NeonReferralConstants.mainTextColor
+        ReferanceCodeLabel.backgroundColor = NeonReferralConstants.textFieldbackgroundcolor
+        ReferanceCodeLabel.layer.borderWidth = 1
+        ReferanceCodeLabel.layer.masksToBounds = true
+        ReferanceCodeLabel.layer.cornerRadius = 12
+        ReferanceCodeLabel.layer.borderColor = NeonReferralConstants.textFieldBordorColor.cgColor
+        ReferanceCodeLabel.textInsets = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 0)
+        view.addSubview(ReferanceCodeLabel)
+        ReferanceCodeLabel.snp.makeConstraints { make in
+            make.bottom.equalTo(inviteButton.snp.top).inset(-16)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(48)
+            make.width.equalToSuperview().multipliedBy(0.9)
+        }
+        copyButton.backgroundColor = NeonReferralConstants.mainColor
+        copyButton.setTitle("Copy", for: .normal)
+        copyButton.setTitleColor(NeonReferralConstants.buttonTextColor, for: .normal)
+        copyButton.layer.masksToBounds = true
+        copyButton.layer.cornerRadius = 4
+        copyButton.addTarget(self, action: #selector(copyButtonClicked), for: .touchUpInside)
+        view.addSubview(copyButton)
+        copyButton.snp.makeConstraints { make in
+            make.centerY.equalTo(ReferanceCodeLabel)
+            make.right.equalTo(ReferanceCodeLabel.snp.right).inset(10)
+            make.width.equalTo(70)
+        }
+        userCodeLabel.text = "User Code: "
+        userCodeLabel.textColor = NeonReferralConstants.mainTextColor
+        userCodeLabel.textAlignment = .left
+        userCodeLabel.font = Font.custom(size: 16, fontWeight: .Regular)
+        view.addSubview(userCodeLabel)
+        userCodeLabel.snp.makeConstraints { make in
+            make.bottom.equalTo(ReferanceCodeLabel.snp.top).inset(-8)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(24)
+            make.width.equalToSuperview().multipliedBy(0.9)
+        }
+    }
+    
+    func fetchCredit(){
+        remainingCredit.text = "Remaining Invite Credits: \(NeonReferralConstants.remainingCredit) "
+    }
+    
+    func gradientConfigure(){
+        var gradientTopColor = UIColor()
+        gradientTopColor = .customBlue.withAlphaComponent(0.2)
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [
+            gradientTopColor.cgColor,
+            NeonReferralConstants.backgroundColor.cgColor
+        ]
+        gradientLayer.startPoint = CGPoint(x: 1, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    func setupNotificationView() {
+        notificationView.backgroundColor = NeonReferralConstants.descriptionColor
+        notificationView.layer.cornerRadius = 16
+        notificationView.isHidden = true
+        view.addSubview(notificationView)
+        notificationView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(50)
+            make.width.equalToSuperview().multipliedBy(0.9)
+            make.height.equalTo(64)
+        }
+        let notificationViewText = UILabel()
+        notificationViewText.text = "Copied to clipboard"
+        notificationViewText.textColor = NeonReferralConstants.mainTextColor
+        notificationViewText.backgroundColor = .clear
+        notificationViewText.font = Font.custom(size: 16, fontWeight: .Regular)
+        notificationView.addSubview(notificationViewText)
+        notificationViewText.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.equalToSuperview().inset(64)
+        }
+        let iconImageView = UIImageView()
+        iconImageView.image = .copied.withRenderingMode(.alwaysTemplate)
+        iconImageView.tintColor = NeonReferralConstants.mainColor
+        notificationView.addSubview(iconImageView)
+        iconImageView.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(20)
+            make.centerY.equalToSuperview()
+            make.width.height.equalTo(24)
+        }
+    }
+    func showNotification() {
+          notificationView.isHidden = false
+          UIView.animate(withDuration: 0.3, animations: {
+              self.notificationView.alpha = 1.0
+          }) { _ in
+              UIView.animate(withDuration: 0.3, delay: 1.0, options: [], animations: {
+                  self.notificationView.alpha = 0.0
+              }) { _ in
+                  self.notificationView.isHidden = true
+                  self.copyButton.isEnabled = true
+              }
+          }
+    }
+    
+    //MARK: Actions
+    @objc func backButtonClicked(){
+        self.dismiss(animated: true)
+    }
+    @objc func copyButtonClicked(){
+        copyButton.isEnabled = false
+        UIPasteboard.general.string = newUserID
+        showNotification()
+    }
+    @objc func inviteButtonClicked(){
+        let activityVC = UIActivityViewController(activityItems: [newUserID], applicationActivities: nil)
+        present(activityVC, animated: true, completion: nil)
+    }
+}
