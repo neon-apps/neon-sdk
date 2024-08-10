f3d565163a8556fef59464bab92eb36cb73a42f6
Tunahan Öztürk <business@tunaozturk.com>
2024-08-10 14:49:02 +0300

    Bug Fixes

--- Neon/NeonReferralManager/Manager/NeonReferralManager.swift
+++ Neon/NeonReferralManager/Manager/NeonReferralManager.swift
@@ -25,15 +25,24 @@ class NeonReferralManager {
     //MARK: presentReddemController
     public static func presentReddemController(from viewController: UIViewController) {
         let reddemVC = CodeUseVC()
-        custom(presentingVC: viewController, destinationVC: reddemVC, height: 224, cornerRadius: 24)
+        reddemVC.modalPresentationStyle = .formSheet
+        if let sheet = reddemVC.sheetPresentationController {
+            sheet.detents = [.custom { _ in
+                return CGFloat(224)
+            }]
+            sheet.preferredCornerRadius = CGFloat(24)
+            sheet.prefersGrabberVisible = true
+        }
+        viewController.present(reddemVC, animated: true, completion: nil)
+        
     }
-
+    
     //MARK: Configure
     private static func setDatabase() {
         guard let userID = AuthManager.currentUserID else { return }
         let db = Firestore.firestore()
         let userRef = db.collection("Users").document(userID)
-
+        
         userRef.getDocument { document, error in
             if let document = document, document.exists {
                 if document.get("Credit") != nil {
@@ -51,15 +60,15 @@ class NeonReferralManager {
             }
         }
     }
-
+    
     public static func configure(prizeAmount: Int,
-                          prizeTerminology: String,
-                          primaryTextColor: UIColor,
-                          mainColor: UIColor,
-                          secondaryTextColor: UIColor,
-                          backgroundColor: UIColor,
-                          containerColor: UIColor,
-                          buttonTextColor: UIColor) {
+                                 prizeTerminology: String,
+                                 primaryTextColor: UIColor,
+                                 mainColor: UIColor,
+                                 secondaryTextColor: UIColor,
+                                 backgroundColor: UIColor,
+                                 containerColor: UIColor,
+                                 buttonTextColor: UIColor) {
         setDatabase()
         
         NeonReferralConstants.prizeAmount = prizeAmount
@@ -70,19 +79,19 @@ class NeonReferralManager {
         NeonReferralConstants.buttonTextColor = buttonTextColor
         NeonReferralConstants.backgroundColor = backgroundColor
         NeonReferralConstants.containerColor = containerColor
-       }
-
+    }
+    
     private static func fetchUserCredit(){
         NeonReferralDatabase.shared.fetchUserCredit() { credit, error in
-               if let error = error {
-                   print("Error fetching document: \(error)")
-               } else if let credit = credit {
-                   print("User credit : \(credit)")
-                   NeonReferralConstants.remainingCredit = Int(credit)
-               } else {
-                   print("Document does not exist or `Credit` field is missing.")
-               }
-           }
+            if let error = error {
+                print("Error fetching document: \(error)")
+            } else if let credit = credit {
+                print("User credit : \(credit)")
+                NeonReferralConstants.remainingCredit = Int(credit)
+            } else {
+                print("Document does not exist or `Credit` field is missing.")
+            }
+        }
     }
     public static func useCredit(amount: Int, completion: @escaping (Bool, String?) -> Void) {
         let currentCredit = NeonReferralConstants.remainingCredit
@@ -101,7 +110,7 @@ class NeonReferralManager {
             completion(true, nil)
         }
     }
-
+    
     public static func earnCredit(amount : Int){
         guard let userID = AuthManager.currentUserID else { return }
         NeonReferralDatabase.shared.addCredit(for: userID, amount: amount)
@@ -109,17 +118,5 @@ class NeonReferralManager {
         print(NeonReferralConstants.remainingCredit)
     }
     
-    private func custom(presentingVC: UIViewController, destinationVC: UIViewController, height: Int , cornerRadius: Int ) {
-        DispatchQueue.main.async {
-            destinationVC.modalPresentationStyle = .formSheet
-            if let sheet = destinationVC.sheetPresentationController {
-                sheet.detents = [.custom { _ in
-                    return CGFloat(height)
-                }]
-                sheet.preferredCornerRadius = CGFloat(cornerRadius)
-                sheet.prefersGrabberVisible = true
-            }
-            presentingVC.present(destinationVC, animated: true, completion: nil)
-        }
-    }
+    
 }
