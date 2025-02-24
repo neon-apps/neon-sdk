f2fd09b3dae4b0161491153e03e0dffe8f980460
Tyler Blackford <business@tunaozturk.com>
2025-02-24 20:59:46 +0300

    Update NeonNotificationCenter.swift

--- Neon/Core/Helpers/NeonNotificationCenter.swift
+++ Neon/Core/Helpers/NeonNotificationCenter.swift
@@ -33,12 +33,42 @@ public class NeonNotificationCenter{
         activeObservers.append((notificationObject,id))
     }
     
+    public static func observe(id : String, preserveActiveObservers : Bool = false, completion: @escaping (_ object : Any?) -> ()){
+        if !preserveActiveObservers{
+            removeObserver(id: id)
+        }
+        
+        var notificationObject = NotificationCenter.observe(name: Notification.Name(rawValue: id)) { notification in
+            if let userInfo = notification.userInfo as? [String : Any], let object = userInfo["neon-object"]{
+                    completion(object)
+            }else{
+                completion(nil)
+            }
+        }
+        activeObservers.append((notificationObject,id))
+
+    }
+    
+    
     public static func observe(id : String, completion: @escaping () -> ()){
         removeObserver(id: id)
         var notificationObject =  NotificationCenter.observe(name: Notification.Name(rawValue: id)) { notification in
             completion()
         }
         activeObservers.append((notificationObject,id))
+    }
+    
+    public static func observe(id : String, preserveActiveObservers : Bool = false, completion: @escaping () -> ()){
+        if !preserveActiveObservers{
+            removeObserver(id: id)
+        }
+        
+        var notificationObject =  NotificationCenter.observe(name: Notification.Name(rawValue: id)) { notification in
+            completion()
+        }
+        
+        activeObservers.append((notificationObject,id))
+
     }
     
     public static func removeObserver(id: String) {
