1808d7e50be1ada53c37f72ae24bc06ee6cc0ab2
Tyler Blackford <business@tunaozturk.com>
2025-03-19 11:58:44 +0300

    Update UIImageView+Extensions.swift

--- Neon/Core/Extensions/UIImageView+Extensions.swift
+++ Neon/Core/Extensions/UIImageView+Extensions.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Tuna Öztürk on 11.03.2023.
 //
@@ -11,14 +11,14 @@ import SDWebImage
 
 
 extension UIImageView{
-   
-    public func setImage(urlString : String){
-    self.sd_imageTransition = .fade
-    self.sd_setImage(with: URL(string: urlString), completed: { [] (image, error, cacheType, url) in })
-  
-  }
     
-    func setImage(urlString: String, completion: @escaping (UIImage?) -> Void) {
+    public func setImage(urlString : String){
+        self.sd_imageTransition = .fade
+        self.sd_setImage(with: URL(string: urlString), completed: { [] (image, error, cacheType, url) in })
+        
+    }
+    
+    public func setImage(urlString: String, completion: @escaping (UIImage?) -> Void) {
         self.sd_imageTransition = .fade
         self.sd_setImage(with: URL(string: urlString)) { image, _, _, _ in
             completion(image)
