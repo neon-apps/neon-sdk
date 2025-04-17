2c4752ce153f913dc162e7bb106ff869a1275dfd
Eymen Varilci <evarilci@gmail.com>
2025-04-17 16:37:34 +0300

    returnString fixer

--- Neon/Core/Extensions/Date+Extensions.swift
+++ Neon/Core/Extensions/Date+Extensions.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Tuna Öztürk on 9.03.2023.
 //
@@ -8,13 +8,18 @@
 import Foundation
 
 extension Date {
-    public func returnString(format : String = "MM/dd/yyyy", timeZone: String? = nil) -> String {
+    public func returnString(format: String = "MM/dd/yyyy", timeZone: String? = nil) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = format
         dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        if let timeZone{
+        
+        // Set time zone if provided, otherwise default to UTC
+        if let timeZone = timeZone {
             dateFormatter.timeZone = TimeZone(identifier: timeZone)
+        } else {
+            dateFormatter.timeZone = TimeZone(identifier: "UTC") // Default to UTC if no time zone is provided
         }
+        
         return dateFormatter.string(from: self)
     }
 }
