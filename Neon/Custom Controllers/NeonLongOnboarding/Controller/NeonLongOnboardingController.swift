7bb9857ed2adf9af54843939fbebe640666dc625
Eymen Varilci <evarilci@gmail.com>
2026-01-28 17:22:03 +0300

    long onboarding navigation added

--- Neon/Custom Controllers/NeonLongOnboarding/Controller/NeonLongOnboardingController.swift
+++ Neon/Custom Controllers/NeonLongOnboarding/Controller/NeonLongOnboardingController.swift
@@ -11,9 +11,19 @@ import UIKit
 @available(iOS 13.0, *)
 open class NeonLongOnboardingController: UIViewController, NeonLongOnboardingManagerProtocol {
   
-   
+    open var requiringNavigationController: Bool = false
+  
+ 
+  
+override public init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+      super.init(nibName: nil, bundle: nil)
+  }
+  
+  required public init?(coder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+  }
+  
     
-
     open override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -40,7 +50,13 @@ open class NeonLongOnboardingController: UIViewController, NeonLongOnboardingMan
         NeonLongOnboardingConstants.currentSection = firstSection
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
-            controller.present(destinationVC: firstPage.controller, slideDirection: .right)
+            if self.requiringNavigationController {
+                if let nav = controller as? UINavigationController {
+                    nav.pushViewController(firstPage.controller, animated: true)
+                }
+            } else {
+                controller.present(destinationVC: firstPage.controller, slideDirection: .right)
+            }
         })
     }
     
